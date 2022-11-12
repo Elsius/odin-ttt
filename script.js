094e73cab0f4d30c90a0b8d2db8f6c3ec120ba7a
Elsius <sourly95@gmail.com>
2022-11-11 17:23:07 -0800

    new/clearBoard init module

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let board = document.getElementById('boardTTT')
+const ticTacToe = (function () {
+    let board = document.getElementById('boardTTT'),
+        boardState = [],
+        newBoard = function () {
+            //create board elements, looped 9x for each button
+            for (i = 0; i < 9; i++) {
+                let card = document.createElement('div');
+                card.textContent = 'x'
+                card.setAttribute('class', 'tttCard')
+                board.appendChild(card)
+            }
+        },
+        clearBoard = function() {
+            board.innerHTML = ''
+        },
+        init = function(){
+            clearBoard()
+            newBoard()
+        };
+    return {init,}
+})()
 
-function clearBoard(){
-    board.innerHTML = ''
-}
-function newBoard(){
-    //create board elements, looped 9x for each button
-    for (i = 0; i < 9; i++){
-        let card = document.createElement('div');
-        card.textContent = 'x'
-        card.setAttribute('class','tttCard')
-        board.appendChild(card)
-    }
-}
