8c0ea3cf081573b1ff5c3c158488ac0a663ea76e
Elsius <sourly95@gmail.com>
2022-11-13 18:03:01 -0800

    onClick and render function clicking logs into boardState render checks it and redraws

--- script.js
+++ script.js
@@ -5,18 +5,55 @@ const ticTacToe = (function () {
             //create board elements, looped 9x for each button
             for (i = 0; i < 9; i++) {
                 let card = document.createElement('div');
+                //variable for index of item = loop?
                 card.textContent = 'x'
-                card.setAttribute('class', 'tttCard')
+                card.classList.add('tttCard')
                 board.appendChild(card)
+                card.addEventListener('click', clickBoard.bind(card, i))
+                boardState.push(0);
             }
         },
-        clearBoard = function() {
+        clearBoard = function () {
             board.innerHTML = ''
+            boardState = []
         },
-        init = function(){
+        clickBoard = function (index) {
+            //index = if 0, change to current player turn, else send error
+            if (boardState[index] == 0) {
+                boardState[index] = 1
+            } else {
+                console.log('this is taken')
+            }
+            render()
+            
+        },
+        render = function () {
+            console.log('render')
+            //read boardState and color board accordingly, then check for wincon
+
+            for (i = 0; i < 9; i++){
+                switch (boardState[i]){
+                    case 0:
+                        continue;
+                    case 1:
+                        //change to p1 class
+                        board.children[i].classList.add('tttX')
+                    break;
+                    case 2:
+                        board.children[i].classList.add('tttO')
+                    break;
+                }
+            }
+            checkWin()
+        },
+        checkWin = function () {
+
+        },
+        init = function () {
             clearBoard()
             newBoard()
         };
-    return {init,}
+    return { init, }
 })()
 
+ticTacToe.init()
