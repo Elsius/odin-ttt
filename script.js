bf77689ff233b2b9d28e922313df66f7253d3b9a
Elsius <sourly95@gmail.com>
2022-11-16 17:19:17 -0800

    2 Players, Diagonal Win Con

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const ticTacToe = (function () {
     let board = document.getElementById('boardTTT'),
         boardState = [],
+        playerTurn = 1,
         newBoard = function () {
             //create board elements, looped 9x for each button
             for (i = 0; i < 9; i++) {
                 let card = document.createElement('div');
-                //variable for index of item = loop?
                 card.textContent = 'x'
                 card.classList.add('tttCard')
                 board.appendChild(card)
@@ -19,41 +19,62 @@ const ticTacToe = (function () {
         },
         clickBoard = function (index) {
             //index = if 0, change to current player turn, else send error
+
             if (boardState[index] == 0) {
-                boardState[index] = 1
+                boardState[index] = playerTurn
+                render()
+                checkWin(index)
             } else {
-                console.log('this is taken')
+                console.log(`section ${index} is taken`)
             }
-            render()
-            
         },
         render = function () {
-            console.log('render')
             //read boardState and color board accordingly, then check for wincon
-
-            for (i = 0; i < 9; i++){
-                switch (boardState[i]){
+            for (i = 0; i < 9; i++) {
+                switch (boardState[i]) {
                     case 0:
                         continue;
                     case 1:
-                        //change to p1 class
                         board.children[i].classList.add('tttX')
-                    break;
+                        break;
                     case 2:
                         board.children[i].classList.add('tttO')
-                    break;
+                        break;
                 }
             }
-            checkWin()
         },
-        checkWin = function () {
+        checkWin = function (indexLastClicked) {
+            //loops through win conditions
+            let win
+            console.log()
+            //check last clicked?
 
+            //check the 3 possible win conditions?
+            //check diagonals
+            if (indexLastClicked % 2 == 0) {
+                if (boardState[0] == playerTurn &&
+                    boardState[4] == playerTurn &&
+                    boardState[8] == playerTurn ||
+                    boardState[2] == playerTurn &&
+                    boardState[4] == playerTurn &&
+                    boardState[6] == playerTurn) {
+                    console.log(`Player ${playerTurn} wins!`)}
+            }
+            
+            if (playerTurn == 1) {
+                playerTurn = 2
+            } else {
+                playerTurn = 1
+            }
         },
         init = function () {
             clearBoard()
             newBoard()
         };
-    return { init, }
+        checkState = function() {
+            console.log(boardState)
+        }
+    return { init, checkState}
 })()
 
 ticTacToe.init()
